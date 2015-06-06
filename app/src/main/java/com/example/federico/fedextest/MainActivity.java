6e22bbee158b026cb52852ddd0825e62ff5749c1
Federico Melet <fmelet@gmail.com>
2015-06-06 18:25:28 +0200

    3

--- app/src/main/java/com/example/federico/fedextest/MainActivity.java
+++ app/src/main/java/com/example/federico/fedextest/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.federico.fedextest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 
 public class MainActivity extends Activity {
@@ -11,7 +14,12 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
+        Crashlytics.getInstance().core.setInt("current_level", 1);
+        Crashlytics.getInstance().core.setString("last_UI_action", "logged_in1");
+        Crashlytics.getInstance().core.log(Log.ERROR, "MyApp2", "Higgs-Boson detected! Bailing out...");
+
     }
 
     @Override
@@ -27,9 +35,18 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
+
+
+
         if (id == R.id.action_settings) {
+            try {
+                throw new RuntimeException("This is a crash");
+            } catch (Exception e) {
+                Crashlytics.getInstance().core.logException(e);
+                // handle your exception here!
+            }
+
             return true;
         }
 
